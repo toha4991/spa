8fa2bed239b5c232a0645c096b1ff1ff7147f33c
Janusz Stal <janusz.stal@uek.krakow.pl>
2019-03-12 18:26:16 +0100

    tylko poczatkowe zajecia

--- 08-SinglePageApp/model.js
+++
@@ -1,6 +0,0 @@
-var model = {
-		getData: function () {return this.storage; },
-		addData: function (data) {this.storage.push(data); },
-		delData: function (code) {},
-		storage: [] // array of objects {code, rate, name}
-	};
